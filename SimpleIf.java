a610b044eb386dcdacb843867eb12096faaeb953
bhaveshrathod741 <sameer.rathod29@gmail.com>
2019-12-03 01:07:58 +0530

    genkins configuration

---
+++ SimpleIf.java
@@ -0,0 +1,19 @@
+//Demo of simple if statement
+
+//package selectionStatement;
+
+public class SimpleIf {
+	
+	public static void main(String args[])
+	{
+		int a =10, b=20;
+		
+		//if condition syntax
+		
+		if(a < b)
+		{
+			System.out.println((a<b));
+		}
+	}
+
+}
